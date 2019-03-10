bdecb02d316553921e8466c9baffefeabba13054
EKG <elliotgallagher91@gmail.com>
2019-03-10 14:17:28 -0400

    Added react-bulma-components and simplified navbar button structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Hero } from 'react-bulma-components';
+
+// import logo from '../images/EKGlogo.jpg'; //TODO: put the logo back in
 
 class Nav extends Component {
+  state={
+    isActive: false,
+  }
+
+  toggleNav = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive
+    }))
+  }
+
   render() {
     return (
-      <div class='navbar is-primary columns'>
-        <div class='navbar-brand column is-half'>
-          <h1 class='has-text-white is-large'>EKG Software Engineering</h1>
-          <div role='button' class='navbar-burger' aria-label='menu' aria-expanded='false'>
+      <div class='navbar has-background-light'>
+        <div class='navbar-brand'>
+          {/*<img src={ logo } alt='logo' class='navbar-item size-2' style={{}} />*/}
+          <h1 class='has-text-dark is-large navbar-item'>EKG Software Engineering</h1>
+          <div role='button' class='navbar-burger' aria-label='menu' aria-expanded='false' onClick={this.toggleNav}>
             <span aria-hidden='true'></span>
             <span aria-hidden='true'></span>
             <span aria-hidden='true'></span>
           </div>
         </div>
-        <div class='navbar-menu column is-half'>
-          <div class='navbar-start'>
-            <div class='button navbar-item'><Link to='/'>Home</Link></div>
-            <div class='button navbar-item'><Link to='/About'>About</Link></div>
-            <div class='button navbar-item'><Link to='/Projects'>Projects</Link></div>
-            <div class='button navbar-item'><Link to='/Resume'>Resumé</Link></div>
-            <div class='button navbar-item'><Link to='/Contact'>Contact</Link></div>
+        <div class={ this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
+          <div class='navbar-end'>
+            <Button
+              renderAs={Link}
+              to='/'
+              color='primary'
+              size='3'
+              style={{ margin:5 }}
+              >Home
+            </Button>
+            <Button
+              renderAs={Link}
+              to='/About'
+              color='primary'
+              size='3'
+              style={{ margin:5 }}
+              >About
+            </Button>
+            <Button
+              renderAs={Link}
+              to='/Projects'
+              color='primary'
+              size='3'
+              style={{ margin:5 }}
+              >Projects
+            </Button>
+            <Button
+              renderAs={Link}
+              to='/Resume'
+              color='primary'
+              size='3'
+              style={{ margin:5 }}
+              >Resumé
+            </Button>
+            <Button
+              renderAs={Link}
+              to='/Contact'
+              color='primary'
+              size='3'
+              rounded
+              outlined
+              style={{ margin:5 }}
+              >Contact
+            </Button>
           </div>
         </div>
       </div>
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Resume from './Resume';
-import Contact from './Contact';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Projects from '../pages/Projects';
+import Resume from '../pages/Resume';
+import Contact from '../pages/Contact';
 
 class View extends Component {
   render () {

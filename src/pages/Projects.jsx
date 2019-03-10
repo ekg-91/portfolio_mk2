61c0741b422ae4a37afebce0510574e6614fa390
EKG <elliotgallagher91@gmail.com>
2019-03-10 14:18:44 -0400

    changed file tree structure

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <h2>PROJECTS PAGE</h2>
+        <h3>Leela, Bender, we're going grave robbing.</h3>
+        <p>That's right, baby. I ain't your loverboy Flexo, the guy you love so much.
+          You even love anyone pretending to be him! For example, if you killed your grandfather,
+          you'd cease to exist! And until then, I can never die?</p>
+      </div>
+    );
+  }
+}
+
+export default Projects;
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <h2>RESUMÉ PAGE</h2>
+        <h2>We're also Santa Claus!</h2>
+        <p>A sexy mistake. Enough about your promiscuous mother, Hermes! We have bigger problems.
+          Bender, I didn't know you liked cooking. That's so cute. You won't have time for sleeping,
+          soldier, not with all the bed making you'll be doing.</p>
+      </div>
+    );
+  }
+}
+
+export default Resume;
